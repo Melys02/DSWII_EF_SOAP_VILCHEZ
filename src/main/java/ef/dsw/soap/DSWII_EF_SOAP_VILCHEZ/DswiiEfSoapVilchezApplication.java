604ecaf10bc85a2085b94405bcdb0a5fe79a42d4
Melys02 <melys0919@gmail.com>
2024-10-22 22:35:12 -0500

    comienzo de proyecto

---
+++ src/main/java/ef/dsw/soap/DSWII_EF_SOAP_VILCHEZ/DswiiEfSoapVilchezApplication.java
@@ -0,0 +1,13 @@
+package ef.dsw.soap.DSWII_EF_SOAP_VILCHEZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiEfSoapVilchezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiEfSoapVilchezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ef/dsw/soap/DSWII_EF_SOAP_VILCHEZ/DswiiEfSoapVilchezApplicationTests.java
@@ -0,0 +1,13 @@
+package ef.dsw.soap.DSWII_EF_SOAP_VILCHEZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiEfSoapVilchezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
